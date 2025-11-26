085a30f238b78d5b7c29f668787b03eaad24fdbf
Fatima Ezzahra <aniberfatimaezzahra@gmail.com>
2025-11-26 16:59:54 +0100

    Initial Calculator project

---
+++ src/test/java/com/fati/calculator/CalculatorTest.java
@@ -0,0 +1,15 @@
+package com.fati.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    @Test
+    public void testAddition() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(2, 3);
+        assertEquals(5, result, "2 + 3 doit être égal à 5");
+    }
+
+}
